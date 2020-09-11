f513a944acc731298d550917534f5830ff9f3375
Rafa Soler <rafasoler10@gmail.com>
2020-09-12 00:34:35 +0200

    Improvements in cookie notice

--- httpdocs/wp-content/themes/generatepress-child/js/custom.js
+++ httpdocs/wp-content/themes/generatepress-child/js/custom.js
@@ -1,15 +1,19 @@
 (function($) {
 
-    /* When the user scrolls down, add the class 'fixed' to the header */
-    function stickyMenu() {
+    /* When the user scrolls down, toggle the classes from header and cookie notice */
+    function stickyElements() {
         const header = document.getElementById('site-navigation');
+        const cookieNotice = document.getElementById('cookie-notice');
+        cookieNotice.classList.add('cookie-notice-custom-hidden');
 
         window.onscroll = function () {
             if (window.pageYOffset > 20) {
                 header.classList.add('fixed');
+                cookieNotice.classList.remove('cookie-notice-custom-hidden');
             }
             else {
                 header.classList.remove('fixed');
+                cookieNotice.classList.add('cookie-notice-custom-hidden');
             }
         }
     }
@@ -19,9 +23,8 @@
 
     $(document).ready(function() {
 
-        stickyMenu();
+        stickyElements();
         
-
     })
 
 })(jQuery);
